6eaa160ef0a01b210059be817659e682ceb2049e
tangusjr <kipngenogilbert96@gmail.com>
2022-08-30 08:45:57 +0300

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,26 @@
 class Person
+  # attr_accessor :name
+  def name name
+      @name=name
+  end
+  def name
+      @name
+  end
+  def num=name
+      @name=name
+  end
+  def job job
+      @job=job
+  end
+  def job
+      @job
+  end
+  def job=job
+      @job=job
+  end
 
 end
+
+beyonce=Person.new
+beyonce.name = "Beyonce"
+beyonce.job = "Singer"
